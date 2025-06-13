f1e2a641b0c0a2cc707963e7dcbe8e71d7c29300
velionn <yukine932@gmail.com>
2025-06-13 18:49:31 +0800

    add companion.nvim

---
+++ .config/nvim/lua/plugins/companion.lua
@@ -0,0 +1,31 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    strategies = {
+      chat = {
+        adapter = "gemini",
+      },
+      inline = {
+        adapter = "gemini",
+      },
+    },
+    adapters = {
+      gemini = function()
+        return require("codecompanion.adapters").extend("gemini", {
+          schema = {
+            model = {
+              default = "gemini-2.5-flash-preview-05-20",
+            },
+          },
+          env = {
+            api_key = "YOUR_KEY_HERE",
+          },
+        })
+      end,
+    },
+  },
+}
